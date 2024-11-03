c07788e6214483109b0357352185a25865c2c39d
marcobt15 <mtoito03@gmail.com>
2024-11-03 15:25:33 -0500

    only q values are acting weird

--- assignment3.py
+++ assignment3.py
@@ -1,53 +1,27 @@
 import numpy as np
 import pandas as pd
 import sys
-import glob
-
-stateToIndex = {
-    'A' : 0,
-    'B' : 1,
-    'C' : 2,
-    'D' : 3,
-    'E' : 4,
-    'F' : 5,
-  }
+import glob #in python standard library
 
 indexToAction = {
-    0 : 'N',
-    1 : 'A',
-    2 : 'B',
-    3 : 'C',
-    4 : 'D',
-    5 : 'E',
-    1 : 'F'
-  }
+  0 : 'N',
+  1 : 'A',
+  2 : 'B',
+  3 : 'C',
+  4 : 'D',
+  5 : 'E',
+  6 : 'F'
+}
 
 actionToIndex = {
-        'N':0,
-        'A':1,
-        'B':2,
-        'C':3,
-        'D':4,
-        'E':5,
-        'F':6
-    }
-
-#creating initial q function
-q_function = {}
-states = ['A', 'B', 'C', 'D', 'E', 'F']
-for mouse_state in states:
-    for cat_state in states:
-        #7 actions
-        new_q_list = []
-        if mouse_state == 'B':
-            new_q_list = 10 * np.ones(7)
-        elif mouse_state == cat_state:
-            new_q_list = -10 * np.ones(7)
-        else:
-            new_q_list = -np.ones(7)
-
-        q_function[mouse_state+cat_state] = new_q_list
-    
+  'N' : 0,
+  'A' : 1,
+  'B' : 2,
+  'C' : 3,
+  'D' : 4,
+  'E' : 5,
+  'F' : 6
+}    
 
 class td_qlearning:
 
@@ -55,44 +29,23 @@ class td_qlearning:
   gamma = 0.95
 
   def __init__(self, directory):
-    self.trials = [[['FE', 'E'], ['ED', 'F'], ['FA', 'E'], ['ED', 'D'], ['DA', 'F'], ['FA', 'N'], ['FD', 'D'], ['DE', 'E'], ['ED', 'F'], ['FA', 'D'], ['DD', '-']], [['EA', 'F'], ['FD', 'N'], ['FD', 'D'], ['DA', 'N'], ['DA', 'E'], ['EB', 'D'], ['DB', 'N'], ['DB', 'N'], ['DB', 'F'], ['FB', 'N'], ['FB', 'D'], ['DB', 'E'], ['EB', 'N'], ['EB', 'F'], ['FB', 'D'], ['DB', 'F'], ['FB', 'D'], ['DB', 'F'], ['FB', 'D'], ['DB', 'F'], ['FB', 'N'], ['FB', 'D'], ['DB', 'N'], ['DB', 'E'], ['EB', 'D'], ['DB', 'A'], ['AB', 'B'], ['BB', '-']], [['FE', 'E'], ['EF', 'D'], ['DE', 'E'], ['EF', 'N'], ['ED', 'N'], ['ED', 'C'], ['CF', 'E'], ['ED', 'C'], ['CD', 'B'], ['BD', '-']], [['EB', 'F'], ['FB', 'N'], ['FB', 'E'], ['EB', 'C'], ['CB', 'B'], ['BB', '-']], [['AB', 'D'], ['DB', 'F'], ['FB', 'E'], ['EB', 'D'], ['DB', 'A'], ['AB', 'D'], ['DB', 'F'], ['FB', 'N'], ['FB', 'E'], ['EB', 'C'], ['CB', 'N'], ['CB', 'E'], ['EB', 'F'], ['FB', 'N'], ['FB', 'N'], ['FB', 'D'], ['DB', 'F'], ['FB', 'D'], ['DB', 'E'], ['EB', 'F'], ['FB', 'N'], ['FB', 'E'], ['EB', 'N'], ['EB', 'C'], ['CB', 'B'], ['BB', '-']], [['ED', 'N'], ['EA', 'D'], ['DD', '-']], [['ED', 'C'], ['CD', 'N'], ['CE', 'E'], ['ED', 'C'], ['CA', 'B'], ['BA', '-']], [['EF', 'D'], ['DF', 'F'], ['FD', 'N'], ['FE', 'E'], ['EC', 'F'], ['FC', 'D'], ['DB', 'N'], ['DB', 'F'], ['FB', 'E'], ['EB', 'D'], ['DB', 'A'], ['AB', 'B'], ['BB', '-']], [['FD', 'D'], ['DF', 'F'], ['FD', 'E'], ['EE', '-']], [['FB', 'N'], ['FB', 'D'], ['DB', 'F'], ['FB', 'N'], ['FB', 'D'], ['DB', 'E'], ['EB', 'N'], ['EB', 'F'], ['FB', 'D'], ['DB', 'F'], ['FB', 'E'], ['EB', 'C'], ['CB', 'B'], ['BB', '-']]]
 
-    for trial in self.trials:
-      for i in range(len(trial)-1):
-          state, action = trial[i]
-          next_state = trial[i+1][0]
-
-          curr_q_value = q_function[state][actionToIndex[action]]
-          reward_next_state = self.reward(next_state)
-          best_next_q = max(q_function[next_state])
-
-          q_function[state][actionToIndex[action]] += self.alpha * (reward_next_state + self.gamma * best_next_q - curr_q_value)
-
-    return
+    #creating initial q function
+    self.q_function = {}
+    states = ['A', 'B', 'C', 'D', 'E', 'F']
+    for mouse_state in states:
+        for cat_state in states:
+            state = mouse_state+cat_state
+            curr_q_value = self.reward(state)
+            self.q_function[state] = [curr_q_value for _ in range(7)]
 
     self.trials = []
     
     for file in glob.glob(f"{directory}/*.csv"):
-      self.trials.append(pd.read_csv(file).values)
+      self.trials.append(pd.read_csv(file, header=None).values)
 
-  def qvalue(self, state, action):
-    # state is a string representation of a state
-    # action is a string representation of an action
-    actionIndex = actionToIndex[action]
+    self.train()
 
-    q = q_function[state][actionIndex]
-
-    # Return the q-value for the state-action pair
-    return q
-
-  def policy(self, state):
-    # state is a string representation of a state
-
-    index = np.argmax(q_function[state])
-    a = indexToAction[index]
-    # Return the optimal action under the learned policy
-    return a
-   
   def reward(self, state):
     if state[0] == 'B': 
         return 10
@@ -101,11 +54,80 @@ class td_qlearning:
     else:
         return -1
 
-test = td_qlearning('')
-print(test.policy('FC'))
-print(test.qvalue('FC','N'))
-print(test.qvalue('FC','E'))
-print(test.qvalue('FC','D'))
+  def train(self):
+    for _ in range(20):
+      for trial in self.trials:
+        for i in range(len(trial)-1):
+          state, action = trial[i]
+          next_state = trial[i+1][0]
 
+          curr_q_value = self.q_function[state][actionToIndex[action]]
+          reward_next_state = self.reward(next_state)
+          best_next_q = max(self.q_function[next_state])
 
-print(test.qvalue('FB','D'))
+          new_q = curr_q_value + self.alpha * (reward_next_state + self.gamma * best_next_q - curr_q_value)
+
+          self.q_function[state][actionToIndex[action]] = new_q
+
+  def qvalue(self, state, action):
+    # state is a string representation of a state
+    # action is a string representation of an action
+    actionIndex = actionToIndex[action]
+    q = self.q_function[state][actionIndex]
+
+    # Return the q-value for the state-action pair
+    return q
+
+  def policy(self, state):
+    # state is a string representation of a state
+    index = np.argmax(self.q_function[state])
+    a = indexToAction[index]
+    # Return the optimal action under the learned policy
+    return a
+  
+  def printNicely(self):
+    for key, value in self.q_function.items():
+      print(key + ": ")
+      actions = ['N', 'A', 'B', 'C', 'D', 'E', 'F']
+      for i, val in enumerate(value):
+        print(actions[i] + ": " + str(val), end=" ")
+      print()
+      print()
+
+test = td_qlearning("Examples\\Example0\\Trials")
+
+print(test.printNicely())
+
+#For example 0
+print(test.policy('EB'))
+print(test.qvalue('DA','E'))
+
+#For example 1
+# print(test.policy('FC'))#D
+# print(test.policy('FC'))#D
+# print(test.policy('AC'))#NBD
+# print(test.policy('FC'))#D
+# print(test.policy('FB'))#DE
+# print(test.policy('AA'))#NBD
+# print(test.policy('AA'))#NBD
+# print(test.policy('EF'))#N
+
+# print(test.qvalue('FC','N'))#-1
+# print(test.qvalue('FC','E'))#-1
+# print(test.qvalue('AC','D'))#-1
+# print(test.qvalue('FC','E'))#-1
+# print(test.qvalue('FB','D'))#5.721249999999985
+# print(test.qvalue('AA','N'))#-10
+# print(test.qvalue('AA','D'))#-10
+# print(test.qvalue('EF','N'))#5.000492195194237
+
+#for example 2
+# print(test.policy('DC'))#A
+# print(test.policy('AE'))#B
+# print(test.policy('DA'))#NAEF
+# print(test.policy('EC'))#C
+
+# print(test.qvalue('DC','E'))#5.721249999999684
+# print(test.qvalue('AE','B'))#8.499999999999993
+# print(test.qvalue('DA','E'))#-1
+# print(test.qvalue('EC','N'))#-10.49999999999982

38514c4d777290ab009ff3178e02d08d716b2491
marcobt15 <mtoito03@gmail.com>
2024-11-08 10:52:40 -0500

    removing invalid actions and clearning up code

--- assignment3.py
+++ assignment3.py
@@ -1,26 +1,5 @@
-import numpy as np
 import pandas as pd
 import glob #in python standard library
-
-indexToAction = {
-  0 : 'N',
-  1 : 'A',
-  2 : 'B',
-  3 : 'C',
-  4 : 'D',
-  5 : 'E',
-  6 : 'F'
-}
-
-actionToIndex = {
-  'N' : 0,
-  'A' : 1,
-  'B' : 2,
-  'C' : 3,
-  'D' : 4,
-  'E' : 5,
-  'F' : 6
-}    
 
 class td_qlearning:
 
@@ -28,15 +7,32 @@ class td_qlearning:
   gamma = 0.95
 
   def __init__(self, directory):
+    #representation of the graph
+    self.graph = {
+        'A': ['B', 'D'], 
+        'B': ['A', 'C'], 
+        'C': ['B', 'E'], 
+        'D': ['A', 'E', 'F'], 
+        'E': ['C', 'D', 'F'], 
+        'F': ['D', 'E']
+    }
 
     #creating initial q function
     self.q_function = {}
     states = ['A', 'B', 'C', 'D', 'E', 'F']
+    actions = ['N', 'A', 'B', 'C', 'D', 'E', 'F']
     for mouse_state in states:
         for cat_state in states:
             state = mouse_state+cat_state
             curr_q_value = self.reward(state)
-            self.q_function[state] = [curr_q_value for _ in range(7)]
+
+            #Only adding actions that are valid
+            current_state_q_values = {}
+            for action in actions:
+              if action == 'N' or action in self.graph[mouse_state]:
+                current_state_q_values[action] = curr_q_value
+            
+            self.q_function[state] = current_state_q_values
 
     self.trials = []
     
@@ -60,77 +56,31 @@ class td_qlearning:
           state, action = trial[i]
           next_state = trial[i+1][0]
 
-          curr_q_value = self.q_function[state][actionToIndex[action]]
+          curr_q_value = self.q_function[state][action]
           current_reward_state = self.reward(state)
-          best_next_q = max(self.q_function[next_state])
+          best_next_q = max(self.q_function[next_state].values())
 
           new_q = curr_q_value + self.alpha * (current_reward_state + self.gamma * best_next_q - curr_q_value)
 
-          self.q_function[state][actionToIndex[action]] = new_q
+          self.q_function[state][action] = new_q
 
   def qvalue(self, state, action):
     # state is a string representation of a state
     # action is a string representation of an action
-    actionIndex = actionToIndex[action]
-    q = self.q_function[state][actionIndex]
 
     # Return the q-value for the state-action pair
-    return q
+    return self.q_function[state][action]
 
   def policy(self, state):
     # state is a string representation of a state
-    index = np.argmax(self.q_function[state])
-    a = indexToAction[index]
+
     # Return the optimal action under the learned policy
-    return a
-  
+    possible_actions = self.q_function[state]
+    best_action = None
+    max_q_value = float('-inf')
+    for key, value in possible_actions.items():
+       if value > max_q_value:
+          best_action = key
+          max_q_value = value
 
-
-
-  ##REMOVE WHEN SUBMITTING
-  def printNicely(self):
-    for key, value in self.q_function.items():
-      print(key + ": ")
-      actions = ['N', 'A', 'B', 'C', 'D', 'E', 'F']
-      for i, val in enumerate(value):
-        print(actions[i] + ": " + str(val), end=" ")
-      print()
-      print()
-
-# test = td_qlearning("Examples\\Example2\\Trials")
-
-# print(test.printNicely())
-
-#For example 0
-# print(test.policy('EB')) #D
-# print(test.qvalue('DA','E')) #2.554585136673036
-
-#For example 1
-# print(test.policy('FC'))#D
-# print(test.policy('FC'))#D
-# print(test.policy('AC'))#NBD
-# print(test.policy('FC'))#D
-# print(test.policy('FB'))#DE
-# print(test.policy('AA'))#NBD
-# print(test.policy('AA'))#NBD
-# print(test.policy('EF'))#N
-
-# print(test.qvalue('FC','N'))#-1
-# print(test.qvalue('FC','E'))#-1
-# print(test.qvalue('AC','D'))#-1
-# print(test.qvalue('FC','E'))#-1
-# print(test.qvalue('FB','D'))#5.721249999999985
-# print(test.qvalue('AA','N'))#-10
-# print(test.qvalue('AA','D'))#-10
-# print(test.qvalue('EF','N'))#5.000492195194237
-
-#for example 2
-# print(test.policy('DC'))#A
-# print(test.policy('AE'))#B
-# print(test.policy('DA'))#NAEF
-# print(test.policy('EC'))#C
-
-# print(test.qvalue('DC','E'))#5.721249999999684
-# print(test.qvalue('AE','B'))#8.499999999999993
-# print(test.qvalue('DA','E'))#-1
-# print(test.qvalue('EC','N'))#-10.49999999999982
+    return best_action
